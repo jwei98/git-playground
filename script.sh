e34d35961e89b69a91165a2c10449bdf907f1015
Justin Wei <justin.lj.wei@gmail.com>
2018-08-08 10:15:54 -0700

    updated script

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-COMMITS="$(git rev-list 0486acb34c870566eee8415b3d6c846baa7fdae6..a31322b59b983b8eb0c8eaeb8d4b5a82705c76e2 --merges)"
+COMMITS="$(git rev-list 0486acb34c870566eee8415b3d6c846baa7fdae6..c20138be931d51e3c4e399f456cfba36e5258f13 --merges)"
 while read -r hash; do
 	INTO="$(git branch --contains $hash)"
 	MSG="$(git log --format=%B -n 1 $hash)"
