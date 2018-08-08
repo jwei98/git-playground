f53ef069207f622ee73aaa36dd0f1f6891825529
Justin Wei <justin.lj.wei@gmail.com>
2018-08-08 10:39:26 -0700

    fixed script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 START=0486acb34c870566eee8415b3d6c846baa7fdae6
-END=0315b06eb07eb045f5216b14ef2a6f0d2dee10f8 
-COMMITS="$(git rev-list $START..$END--merges)"
+END=042049fc72447d2d8e7e05b09320b9bcc2428185
+COMMITS="$(git rev-list $START..$END --merges)"
 
 while read -r hash; do
 	MSG="$(git log --format=%B -n 1 $hash)"
